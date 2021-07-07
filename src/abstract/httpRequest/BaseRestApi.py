3987fcde19a4d2573cd0b0b6ce3d959f58b6fcab
Amine Benkhouya <edu.amine@gmail.com>
2021-07-08 00:01:34 +0200

    ascendex http rest implementation

--- src/abstract/httpRequest/BaseRestApi.py
+++ src/abstract/httpRequest/BaseRestApi.py
@@ -3,22 +3,21 @@
 
 import json
 import requests
-from uuid import uuid1
 from urllib.parse import urljoin
+from abc import ABC, abstractmethod
 
 
-class BaseRestApi(object):
+class BaseRestApi(ABC):
 
     def __init__(self, key, secret, url):
         self.url = url
         self.key = key
         self.secret = secret
 
-    def _headers(self):
-        raise NotImplementedError("header by exchange specifications")
+    def _headers(self, headerMeta=None):
+        raise NotImplementedError("headers done by exchange specifications")
 
-    def _request(self, method, uri, timeout=5, auth=True, params=None):
-        uri_path = uri
+    def _request(self, method, uri, timeout=5, auth=True, params=None, headerMeta=None):
         data_json = ''
         if method in ['GET', 'DELETE']:
             if params:
@@ -27,13 +26,11 @@ class BaseRestApi(object):
                     strl.append("{}={}".format(key, params[key]))
                 data_json += '&'.join(strl)
                 uri += '?' + data_json
-                uri_path = uri
         else:
             if params:
                 data_json = json.dumps(params)
-                uri_path = uri + data_json
-
-        headers = self._headers()
+        print(headerMeta)
+        headers = self._headers(headerMeta)
         url = urljoin(self.url, uri)
         if method in ['GET', 'DELETE']:
             response_data = requests.request(
@@ -42,6 +39,3 @@ class BaseRestApi(object):
             response_data = requests.request(method, url, headers=headers, data=data_json,
                                              timeout=timeout)
         return self.check_response_data(response_data)
-
-    def check_response_data(response_data):
-        raise NotImplementedError("response handler depends on the exchange")
--- src/ascendex/AscendexRestApi.py
+++ src/ascendex/AscendexRestApi.py
@@ -7,18 +7,41 @@ import time
 
 class AscendexRestApi(BaseRestApi):
     def __init__(self, key, secret, group, url):
-        super().__init__(self, key, secret, url)
+        super().__init__(key, secret, url)
         self.group = group
 
-    def _headers(self):
+    def _headers(self, headerMeta):
         now_time = int(time.time()) * 1000
-        str_to_sign = str(now_time) + method + uri_path
-        passphrase = base64.b64encode(
-            hmac.new(self.secret.encode('utf-8'), self.passphrase.encode('utf-8'), hashlib.sha256).digest())
+        apiPath = 'info'
+        str_to_sign = str(now_time) + "+" + headerMeta['path']
+        print(str_to_sign)
+        signature = base64.b64encode(
+            hmac.new(self.secret.encode('utf-8'), str_to_sign.encode('utf-8'), hashlib.sha256).digest())
         return {
-            "x-auth-signature": sign,
+            "x-auth-signature": signature,
             "x-auth-timestamp": str(now_time),
             "x-auth-key": self.key,
             "Content-Type": "application/json",
             "Accept": "application/json",
         }
+
+    @staticmethod
+    def check_response_data(response_data):
+        if response_data.status_code == 200:
+            try:
+                data = response_data.json()
+            except ValueError:
+                raise Exception(response_data.content)
+            else:
+                if data and 'code' in data:
+                    if data.get('code') == 0:
+                        if data.get('data'):
+                            return data['data']
+                        else:
+                            return data
+                    else:
+                        raise Exception(
+                            "{}-{}".format(response_data.status_code, response_data.text))
+        else:
+            raise Exception(
+                "{}-{}".format(response_data.status_code, response_data.text))
